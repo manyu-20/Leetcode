ea5357acdd0d2dc89a80c83420ff70c0221bb6a2
Abhimanyu Saini <69662439+manyu-20@users.noreply.github.com>
2021-07-21 13:34:55 +0530

    Time: 4 ms (34.52%), Space: 41.7 MB (32.18%) - LeetHub

---
+++ grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        int satisfy = 0;
+        for(int i=0;i<grumpy.length;i++){
+            if(grumpy[i]==0) satisfy += customers[i]; // directly sum the satisfy customers
+            customers[i] = grumpy[i]*customers[i]; // collect the unsatisfy customers
+        }
+        return satisfy + getMax(customers,X);
+    }
+    
+    public int getMax(int[] arr, int X){ // X is window size
+        int max = 0;
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+            if(i>=X){
+                sum -= arr[i-X]; 
+            }
+            max = Math.max(max,sum);
+        }
+        return max;
+    }
+}
