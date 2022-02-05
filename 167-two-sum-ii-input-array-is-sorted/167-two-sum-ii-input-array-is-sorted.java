4084f67f7de7a9bdab0d35e0a2ac4c49783649ea
Abhimanyu Saini <69662439+manyu-20@users.noreply.github.com>
2022-02-05 23:24:28 +0530

    Time: 1 ms (73.00%), Space: 45.3 MB (5.60%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,28 +1,21 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
-        for(int i = 0;i<n;i++){
-            int res = search(i+1,n-1,numbers,target - numbers[i]);
-            if(res != -1){
-                return new int[]{i+1,res+1};
+    public int[] twoSum(int[] num, int target) {
+        int n = num.length;
+        int i = 0;
+        int j = n-1;
+        while(i < j){
+            int sum = num[i] + num[j];
+            if(sum == target){
+                return new int[]{i+1,j+1};
+            }
+            else if(sum < target){
+                i++;
+            }
+            else{
+                j--;
             }
         }
+        
         return new int[]{-1,-1};
-    }
-    
-    private int search(int low,int high,int[] ans,int target){
-        if(low > high){
-            return -1;
-        }
-        int mid = (low + high)/2;
-        if(ans[mid] == target){
-            return mid;
-        }
-        else if(target < ans[mid]){
-            return search(low,mid-1,ans,target);
-        }
-        else{
-            return search(mid+1,high,ans,target);
-        }
     }
 }
