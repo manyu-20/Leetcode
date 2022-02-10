e4f5ef2396f984363720f7ebf600694cecf86172
Abhimanyu Saini <69662439+manyu-20@users.noreply.github.com>
2022-02-10 22:21:28 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (5.14%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,25 +1,32 @@
 class Solution {
-    int[] dp;
     public int rob(int[] nums) {
-        dp = new int[nums.length + 1];
-        Arrays.fill(dp,-1);
-        return sum(0,nums);
+        // return fun(nums.length-1,nums);
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
         
+        for(int i = 1;i<n;i++){
+            int left = nums[i];
+            if(i - 2 >=0){
+                left = left + dp[i-2];
+            }
+            int right = dp[i-1];
+            dp[i] = Math.max(left,right);
+        }
+        
+        return dp[n-1];
     }
     
-    int sum(int index,int[] nums){
-        if(index >= nums.length){
-            return 0;
+    int fun(int index,int[] nums){
+        if(index == 0){
+            return nums[index];
         }
-        if(dp[index] != -1){
-            return dp[index];
+        int left = 0;
+        if(index - 2 >= 0){
+        left = fun(index - 2,nums) + nums[index];
         }
-        int take = nums[index];
-        
-        take = take + sum(index + 2,nums);
-        
-        int dontTake = sum(index + 1,nums);
-        
-        return dp[index] = Math.max(take,dontTake);
+        int right = fun(index - 1,nums);
+        return Math.max(left,right);
     }
+    
 }
