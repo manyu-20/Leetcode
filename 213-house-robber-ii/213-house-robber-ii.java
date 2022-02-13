1f167ddb7f8490e259495c04cad927add5600480
Abhimanyu Saini <69662439+manyu-20@users.noreply.github.com>
2022-02-13 11:39:39 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (21.05%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -2,27 +2,25 @@ class Solution {
     int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        
         if(n == 1){
             return nums[0];
         }
-        dp = new int[n];
+        dp = new int[n+1];
         Arrays.fill(dp,-1);
-        int first = fun(0,nums,n-1);
+        int first = fun(n,1,nums);
         Arrays.fill(dp,-1);
-        int second = fun(1,nums,n);
-        
-        return Math.max(first,second);
+        int sec = fun(n-1,0,nums);
+        return Math.max(first,sec);
         
     }
     
-    int fun(int index,int[] nums,int n){
-        if(index >= n){
+    int fun(int n,int end,int[] nums){
+        if(n <= end){
             return 0;
         }
-        if(dp[index] != -1){
-            return dp[index];
+        if(dp[n] != -1){
+            return dp[n];
         }
-        return dp[index] = Math.max(nums[index]+fun(index+2,nums,n), fun(index+1,nums,n));
+        return dp[n] = Math.max(nums[n-1] + fun(n-2,end,nums),fun(n-1,end,nums));
     }
 }
