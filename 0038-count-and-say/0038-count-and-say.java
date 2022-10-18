dda94eb2d49c69cfc2799030462781179f6c901b
Abhimanyu Saini <69662439+manyu-20@users.noreply.github.com>
2022-10-18 22:19:43 +0530

    Time: 35 ms (30.25%), Space: 50.9 MB (28.93%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -8,34 +8,21 @@ class Solution {
             return start;
         }
         StringBuilder sb = new StringBuilder("");
-        Stack<Integer> st = new Stack<>();
-        st.push(-1);
-        for(int itr = 0;itr<start.length();itr++){
-            String val = String.valueOf(start.charAt(itr));
-            int num = Integer.parseInt(val);
-            st.push(num);
-        }
 
+        int index = 0;
         while(true){
-            int val = st.peek();
+            int val = Integer.parseInt(String.valueOf(start.charAt(index)));
             int count = 0;
-            while(!st.isEmpty() && st.peek() == val){
-                st.pop();
+            while(index < start.length() && val == Integer.parseInt(String.valueOf(start.charAt(index)))){
                 count++;
+                index++;
             }
-            String temp = count +""+val;
-            int index = sb.length() - 1;
-            // System.out.println( start + "  " + temp);
-            // if(index < 0){
-            //     index = 0;
-            // }
-            sb.insert(0,temp);
-            if(st.size()<= 1){
+            sb.append(count+""+val);
+            if(index >= start.length()){
                 break;
             }
         }
 
-        // System.out.println( i + "  " + sb);
         return fun(i+1,n,sb.toString());
         
         
